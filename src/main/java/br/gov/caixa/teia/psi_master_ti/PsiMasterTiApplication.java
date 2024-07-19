f0b4b99aaa37d4bcaaa8cc2e096610bea046f974
Renato Lopes <renato.lopes@extreme.digital>
2024-07-19 00:57:56 -0300

    Commit inicial

---
+++ src/main/java/br/gov/caixa/teia/psi_master_ti/PsiMasterTiApplication.java
@@ -0,0 +1,13 @@
+package br.gov.caixa.teia.psi_master_ti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsiMasterTiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsiMasterTiApplication.class, args);
+	}
+
+}
